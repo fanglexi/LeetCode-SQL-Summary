edb37361dd10b78893fa6a5cdd4db621f4862309
fanglexi <92765836+fanglexi@users.noreply.github.com>
2022-05-26 20:05:02 -0400

    Update 578_Get_Highest_Answer_Rate_Question.sql

--- Basics/578_Get_Highest_Answer_Rate_Question.sql
+++ Basics/578_Get_Highest_Answer_Rate_Question.sql
@@ -1,7 +1,8 @@
 -- Note: answer rate = answer actions / total non-answer actions
 -- If LeetCode change answer for the test case, plz let me know
 
-SELECT TOP 1 question_id AS survey_log
-FROM survey_log
+SELECT question_id AS survey_log
+FROM SurveyLog
 GROUP BY question_id
-ORDER BY COUNT(answer_id)*1.0/(COUNT(*)-COUNT(answer_id)) DESC;
+ORDER BY sum(case when action='answer' then 1 else 0 end)*1.0/(sum(case when action='show' then 1 else 0 end)) DESC, question_id ASC
+Limit 1;
